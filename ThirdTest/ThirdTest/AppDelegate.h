e83c708cbaa86313d5e4a3a300309a4455dd3275
huangwei <you_can_you_up@126.com>
2017-02-22 15:54:09 +0800

    新建工程

---
+++ ThirdTest/ThirdTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdTest
+//
+//  Created by 黄卫 on 17/2/22.
+//  Copyright © 2017年 huangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
